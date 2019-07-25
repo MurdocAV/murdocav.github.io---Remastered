f953fd1dee48bd46a51fbf6eaaee485f4ee07fb5
MurdocAV <azaan.virk1@gmail.com>
2019-07-25 16:33:52 +1200

    Set up new page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <h1>Hello Mr Murdoc!</h1>
-  )
+class App extends React.Component {
+  
+  render () {
+    return (
+      <React.Fragment>
+        <h1 className="name">MurdocAV</h1>
+        <img className="profilePhoto" src="https://via.placeholder.com/150" alt="Photo of a developer #KanyeWest2020"/>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App
