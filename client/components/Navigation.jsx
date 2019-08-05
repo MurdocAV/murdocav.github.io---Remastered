48d91b8b81042ba98c703f7d25022f852ddbb7f3
MurdocAV <azaan.virk1@gmail.com>
2019-08-05 18:26:31 +1200

    M-u.i. grid setup

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import {NavLink, IndexLink} from 'react-router-dom'
+import {Grid} from '@material-ui/core'
 
 class Navigation extends React.Component {
 
     render() {
         return (
             <div className="header">
+                       <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+          >
                 <div className="nav">
                     <NavLink className="projectsLink" activeClassName="active" to={'/Projects'}>Projects</NavLink>
                     <NavLink
@@ -14,6 +21,7 @@ class Navigation extends React.Component {
                         to={'/Home'}>Home</NavLink>
                     <NavLink className="documentLink" activeClassName="active" to={'/Documentation'}>Documentation</NavLink>
                 </div>
+          </Grid>
                 <div className="title">
                     <h1 className="titleHeader">MurdocAV</h1>
                 </div>
