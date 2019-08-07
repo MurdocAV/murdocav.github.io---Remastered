fef135638d0fb17e98d7b283eff638ab3d9122a9
MurdocAV <azaan.virk1@gmail.com>
2019-08-07 16:32:45 +1200

    new formating

--- client/components/tileData.js
+++ client/components/tileData.js
@@ -1,37 +1,11 @@
 const tileData = [
   {
     img: '/images/image1.jpg',
-    title: 'Breakfast',
-    author: 'jill111',
+    title: 'Luke Mitchells',
+    author: 'MurdocAV',
     cols: 2,
-    featured: true,
-  },
-  {
-    img: '/images/image2.jpg',
-    title: 'Tasty burger',
-    author: 'director90',
-  },
-  {
-    img: './images/image3.jpg',
-    title: 'Camera',
-    author: 'Danson67',
-  },
-  {
-    img: './images/image4.jpg',
-    title: 'Morning',
-    author: 'fancycrave1',
-    featured: true,
-  },
-  {
-    img: './images/image5.jpg',
-    title: 'Hats',
-    author: 'Hans',
-  },
-  {
-    img: './images/image6.jpg',
-    title: 'Honey',
-    author: 'fancycravel',
-  },
+    featured: true
+  }
 ]
 
 export default tileData
