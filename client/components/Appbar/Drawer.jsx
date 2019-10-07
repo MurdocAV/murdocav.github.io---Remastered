51c21e602c0ab8b872f433764bcdd3ad46069149
MurdocAV <azaan.virk1@gmail.com>
2019-10-07 14:40:37 +1300

    removed unused code

--- client/components/Appbar/Drawer.jsx
+++ client/components/Appbar/Drawer.jsx
@@ -8,7 +8,6 @@ import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-import Badge from '@material-ui/core/Badge';
 import ListItemText from '@material-ui/core/ListItemText';
 import {HomeRounded, ShopRounded, ClassRounded, AccountBalanceWalletRounded, RecordVoiceOverRounded, FaceRounded, PermMediaRounded, EmailRounded} from '@material-ui/icons'
 
