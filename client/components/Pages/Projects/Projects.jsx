dadd156ec194fce7a36c0f577ceadc6b188e8217
murdocav <azaan.virk1@gmail.com>
2019-11-14 16:37:21 +1300

    added link CV download

--- client/components/Pages/Projects/Projects.jsx
+++ client/components/Pages/Projects/Projects.jsx
@@ -9,6 +9,11 @@ class Projects extends React.Component {
           <a className='noDecoration aTag' href='https://github.com/MurdocAV' key={'github'} replace={true}>
             <img className='gitHubLogo grow' src="images/GitHubLogo.png" alt="GitHub Logo"/>
           </a>
+          <div>
+          <a className='noDecoration aTag iconbyfreepik' href='/Azaan Virk.pdf' key={'github'} replace={true} download>
+            <img className='cvLogo grow' src="images/CV.png" alt="CV Logo"/>
+          </a>
+        </div>
         </div>
         <div className='imageDrawer'>
           <ImageDrawer></ImageDrawer>
