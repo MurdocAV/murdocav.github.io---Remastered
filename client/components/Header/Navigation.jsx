5428e36746e3d9ece21ea1b63935643c6a668523
MurdocAV <azaan.virk1@gmail.com>
2019-12-10 14:32:36 +1300

    Scss styling

--- client/components/Header/Navigation.jsx
+++ client/components/Header/Navigation.jsx
@@ -10,7 +10,7 @@ class Navigation extends React.Component {
                     <h1 className="titleHeader">Azaan Virk</h1>
                 </div>
                 <div className="nav">
-                    <NavLink className="home Link" activeClassName="active" to={'/Home'}>Home</NavLink>
+                    <NavLink className="home Link" activeClassName="active" to={'/'}>Home</NavLink>
                     <NavLink
                         className="about Link"
                         activeClassName="active"
