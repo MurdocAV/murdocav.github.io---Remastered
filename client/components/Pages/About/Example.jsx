a21deec1858136893248857bb7513b481d6fa495
MurdocAV <azaan.virk1@gmail.com>
2019-12-27 12:05:33 +1300

    Working on About Example

--- client/components/Pages/About/Example.jsx
+++ client/components/Pages/About/Example.jsx
@@ -2,11 +2,25 @@ import React from 'react';
 import Carousel from 'react-material-ui-carousel'
 import Paper from '@material-ui/core'
  
-function Example(props)
+
+const Item = function (props) {
+    return (
+        <Paper>
+            <h2>{props.item.name}</h2>
+            <p>{props.item.description}</p>
+ 
+            <Button className="CheckButton">
+                Check it out!
+            </Button>
+        <Paper/>
+    )
+}
+
+export default function Example(props)
 {
     var items = [
         {
-            name: "Random Name #1"
+            name: "Random Name #1",
             description: "Probably the most random thing you have ever seen!"
         },
         {
@@ -23,19 +37,5 @@ function Example(props)
                 })
             }
         <Carousel/>
-    )
-}
- 
-function Item(props)
-{
-    return (
-        <Paper>
-            <h2>{props.item.name}</h2>
-            <p>{props.item.description}</p>
- 
-            <Button className="CheckButton">
-                Check it out!
-            </Button>
-        <Paper/>
     )
 }
