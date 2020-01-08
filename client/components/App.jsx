a3cb6de5e59cd89843a91f80eff65ab3a3bc06d9
MurdocAV <azaan.virk1@gmail.com>
2020-01-09 12:15:57 +1300

    package.json updated dependencies

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -16,15 +16,6 @@ import Header from './Appbar/Header'
 
 import {activePage, goToPage} from '../actions/index'
 
-
-//TODO: Test out Material U.I.; 
-//  1. Use Material's grid to make layered mobile first Responsive U.I.
-//  1.1. Allow user to see the mobile view on desktop
-//  2. Use 'Drawer' component to switch between different components(pages)
-//  S1. Optional (Strech) --> Use breadcrumbs to do complex navigations.
-//  S2. (Strech) --> Advanced Grid list with titlebars (images of worked projects)
-
-
 class App extends React.Component {
 
   // Working for modifying state in the redux store but does not actually go to any pages.
