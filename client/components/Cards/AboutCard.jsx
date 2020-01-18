9148861ffdc79bcdfba03c3ecf8e784d22cc1463
MurdocAV <azaan.virk1@gmail.com>
2020-01-18 14:24:40 +1300

    new updates

--- client/components/Cards/AboutCard.jsx
+++ client/components/Cards/AboutCard.jsx
@@ -32,14 +32,43 @@ export default function AboutCard(props) {
   return (
     <Card className={`classes.card cardMUI about`}>
       <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          {props.title}
-        </Typography>
         <Typography variant="h5" component="h2">
-          {props.cardText}
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-         {props.footerText}
+        <strong className="strongTag">Strengths and Limitations</strong>
+        <br/>
+        My strengths are based on content that I'm not familiar with yet. I enjoy being an active learner so time-management
+        would be a big important technique in order to make sure it's a strength and not a limitation.
+        <br/>
+        <strong className="strongTag">Routine and Structure</strong>
+        <br/>
+        Working with others productively requires having manners, respect and effective concise communication.
+        I believe it's essentail to be able to get along with your peers if you are to be sucessful in any field.
+        <br/>
+        <strong className="strongTag">Getting Unstuck</strong>
+        <br/>
+        When it comes to asking for help, I don't like imposing an issue as a demand. Friendliness is a good way
+        to combat this problem. If someone seems agitated either offer a reasonable solution or leave that
+        person alone.
+        <br/>
+        <strong className="strongTag">Winding down</strong>
+        <br/>
+        Friends and family are an important commitment for me, needing dedicated time to outside work. I have
+        found that you can't get through all the content without breaks. It's important, essential to come at the
+        problem with a fresh set of eyes to gain insight that you didn't have previously.
+        <br/>
+        <strong className="strongTag">Attention and Emotional Intelligence</strong>
+        <br/>
+        Being able to be aware of, control, and express one's emotions is a great skill. You need to handle
+        interpersonal relationships judiciously and empathetically. To do this I find it helpful to be aware of
+        yourself, your body, your emotions, your presence. This can help you better organize your thoughts and
+        attention.
+        <br/>
+        <strong className="strongTag">Learning</strong>
+        <br/>
+        We are constantly being changed by our experiences. You aren't hardwired to be specifically smart at a
+        set skill when you're born. Pracitce, engaging cognitively, improving your skills is vital. Put the
+        information you're learning in context with other informatino you already possess, build a foundation of
+        knowledge. Abilities are developed through effort and difficulty. Talents are not fixed, not giving up easily.
+        Practice, be patient, learn from mistakes. I'm not there yet.
         </Typography>
         <Typography variant="body2" component="p">
         </Typography>
--- client/components/Pages/About/About.jsx
+++ client/components/Pages/About/About.jsx
@@ -3,24 +3,10 @@ import AboutCard from '../../Cards/AboutCard'
 
 class About extends React.Component {
     render() {
-      const text = "Personally while I absolutely hate looking at a block of work, sometimes thinking I'd never be able to finish it. \
-                    I have found that tasks are often no where near as bad as I thought to complete. If the task is too hard \
-                    to tackle, taking breaks is a good way to clear your head and stay present. If I found myself in a \
-                    problem that occurred too often, i.e. Not putting in a ';', creating useful habits that solve the problem \
-                    from occuring would be my solution. \
-                    Continuing to improve beyond the level I'm currently at will be my next tough challenge I'm \
-                    heading into. Core skills are more important than technical, if you can't actually get the job \
-                    there's no point being able to do the task."
-        const title = "About Me"
-        const footer = "More of a reflection than an about me."
 
         return (
             <div className="mainComponent">
-              <AboutCard 
-              cardText={text}
-              title={title}
-              footerText={footer}
-              />
+              <AboutCard />
             </div>
         )
     }
