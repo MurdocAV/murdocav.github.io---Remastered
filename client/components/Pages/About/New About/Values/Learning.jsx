f30e2a1728653e7b9002f2bb856a7f57f82f1728
MurdocAV <azaan.virk1@gmail.com>
2020-01-22 12:56:58 +1300

    Added values

--- client/components/Pages/About/New About/Values/Learning.jsx
+++ client/components/Pages/About/New About/Values/Learning.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
@@ -17,28 +14,25 @@ const useStyles = makeStyles({
   },
 });
 
-export default function CardC() {
+export default function StrengthsLimitations() {
   const classes = useStyles();
 
   return (
     <Card className={`${classes.card} cardInternal`}>
-      <a className='noDecoration aTag' href='https://github.com/MurdocAV/PokeName' key={'PokeName'} replace={true}>
         <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image="/images/PokeName.png"
-            title="PokeName"
-          />
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
-              PokeName
+              Learning
             </Typography>
             <Typography variant="body2" color="textSecondary" component="p">
-              A quick React project that allows users to enter their names and get a Pokemon version of their name displayed in real-time.
+              We are constantly being changed by our experiences. You aren't hardwired to be specifically smart at a
+              set skill when you're born. Pracitce, engaging cognitively, improving your skills is vital. Put the
+              information you're learning in context with other informatino you already possess, build a foundation of
+              knowledge. Abilities are developed through effort and difficulty. Talents are not fixed, not giving up easily.
+              Practice, be patient, learn from mistakes. I'm not there yet.
             </Typography>
           </CardContent>
         </CardActionArea>
-      </a>
     </Card>
   );
 }
