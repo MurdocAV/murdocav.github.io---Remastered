f7807b716111b983da0e5eedf9c384e5eb25a17d
MurdocAV <azaan.virk1@gmail.com>
2020-01-21 16:02:04 +1300

    new about component working

--- client/components/Pages/About/NewAbout.jsx
+++ client/components/Pages/About/NewAbout.jsx
@@ -1,12 +1,13 @@
-import React from 'React'
+import React from 'react'
 import Card from '@material-ui/core/Card'
 
-class NewAbout extends React.Component {
+export default class NewAbout extends React.Component {
   render() {
 
       return (
           <div className="mainComponent">
             <h1>Hello world</h1>
+            <h3>Testing, testing</h3>
           </div>
       )
   }
