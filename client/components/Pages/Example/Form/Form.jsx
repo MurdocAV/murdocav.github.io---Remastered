017f33967a6945496b20fdeedbf5a19b2f5158d1
MurdocAV <azaan.virk1@gmail.com>
2020-02-13 12:46:15 +1300

    colour changed to green

--- client/components/Pages/Example/Form/Form.jsx
+++ client/components/Pages/Example/Form/Form.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { makeStyles, createMuiTheme } from '@material-ui/core/styles'
 import { ThemeProvider } from '@material-ui/styles'
 import TextField from '@material-ui/core/TextField'
-import { common, indigo } from '@material-ui/core/colors'
+import { green, common, indigo } from '@material-ui/core/colors'
 import DialogSelect from './Select'
 import OutlinedButton from './Submit'
 
@@ -20,7 +20,7 @@ const useStyles = makeStyles(theme => ({
 
 const theme = createMuiTheme({
   palette: {
-    primary: common.white,
+    primary: green,
     secondary: indigo,
   },
 })
