5e1f3dedb614acefc7074834addb58783fb46b48
MurdocAV <azaan.virk1@gmail.com>
2020-08-03 18:01:20 +1200

    working switch in react state

--- client/components/Switches/BasicSwitch.jsx
+++ client/components/Switches/BasicSwitch.jsx
@@ -1,45 +1,51 @@
 import React from 'react';
 import Switch from '@material-ui/core/Switch';
 
-export default function Switches() {
-  const [state, setState] = React.useState({
+export default class Switches extends React.Component {
+  
+  state = {
     checkedA: true,
     checkedB: false,
-  });
-  const handleChange = (event) => {
-    setState({ ...state, [event.target.name]: event.target.checked });
+    checkedC: false
+  }
+
+  handleChange (event) {
+    this.setState({ [event.target.name]: event.target.checked });
   };
 
-  return (
-    <div 
-      style={{
-        backgroundColor: 'white',
-        margin: 'auto',
-      }}
-    >
-      <div className='Classic Neon'>
-        <Switch
-        checked={state.checkedA}
-        onChange={handleChange}
-        name="checkedA"
-        inputProps={{ 'aria-label': 'secondary checkbox' }}
-        />
-        <h2 color='pink'>Classic Neon</h2> 
+  
+  render() {
+    return (
+      <div 
+        style={{
+          backgroundColor: 'white',
+          margin: 'auto',
+        }}
+      >
+        <div className='Classic Neon'>
+          <Switch
+          checked={this.state.checkedA}
+          onChange={(e) => { this.handleChange(e) }}
+          name="checkedA"
+          inputProps={{ 'aria-label': 'secondary checkbox' }}
+          />
+          <h2 color='pink'>Classic Neon</h2> 
+        </div>
+        <div className='Orange Sherbet'>
+          <Switch
+          checked={this.state.checkedB}
+          onChange={(e) => { this.handleChange(e) }}
+          color="primary"
+          name="checkedB"
+          inputProps={{ 'aria-label': 'primary checkbox' }}
+          />
+          <h2 color='orange'>Orange Sherbet</h2> 
+        </div>
+        <div className='Herbal Meadows'>
+          <Switch inputProps={{ 'aria-label': 'primary checkbox' }} onChange={(e) => { this.handleChange(e) }}/>
+          <h2 color='orange'>Herbal Meadows</h2> 
+        </div>
       </div>
-      <div className='Orange Sherbet'>
-        <Switch
-        checked={state.checkedB}
-        onChange={handleChange}
-        color="primary"
-        name="checkedB"
-        inputProps={{ 'aria-label': 'primary checkbox' }}
-        />
-        <h2 color='orange'>Orange Sherbet</h2> 
-      </div>
-      <div className='Herbal Meadows'>
-        <Switch inputProps={{ 'aria-label': 'primary checkbox' }} />
-        <h2 color='orange'>Herbal Meadows</h2> 
-      </div>
-    </div>
-  );
+    );
+  }
 }
