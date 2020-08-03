1b6fd3633d84debf5db25361c38d1f5127e54c8c
MurdocAV <azaan.virk1@gmail.com>
2020-08-03 14:16:53 +1200

    added basic switch for theme reducer #10

--- client/components/Appbar/Drawer.jsx
+++ client/components/Appbar/Drawer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 import AppBar from './AppBar'
-
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
