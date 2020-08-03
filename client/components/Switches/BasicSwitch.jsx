f6acfefccb006857011434fe9e4498e37b1eb704
MurdocAV <azaan.virk1@gmail.com>
2020-08-03 15:09:56 +1200

    named themes added for #10

--- client/components/Switches/BasicSwitch.jsx
+++ client/components/Switches/BasicSwitch.jsx
@@ -4,9 +4,8 @@ import Switch from '@material-ui/core/Switch';
 export default function Switches() {
   const [state, setState] = React.useState({
     checkedA: true,
-    checkedB: true,
+    checkedB: false,
   });
-
   const handleChange = (event) => {
     setState({ ...state, [event.target.name]: event.target.checked });
   };
@@ -16,30 +15,31 @@ export default function Switches() {
       style={{
         backgroundColor: 'white',
         margin: 'auto',
-        width: '50%'
       }}
     >
-      <Switch
+      <div className='Classic Neon'>
+        <Switch
         checked={state.checkedA}
         onChange={handleChange}
         name="checkedA"
         inputProps={{ 'aria-label': 'secondary checkbox' }}
-      />
-      <Switch
+        />
+        <h2 color='pink'>Classic Neon</h2> 
+      </div>
+      <div className='Orange Sherbet'>
+        <Switch
         checked={state.checkedB}
         onChange={handleChange}
         color="primary"
         name="checkedB"
         inputProps={{ 'aria-label': 'primary checkbox' }}
-      />
-      <Switch inputProps={{ 'aria-label': 'primary checkbox' }} />
-      <Switch inputProps={{ 'aria-label': 'checkbox' }} />
-      <Switch inputProps={{ 'aria-label': 'primary checkbox' }} />
-      <Switch
-        defaultChecked
-        color="default"
-        inputProps={{ 'aria-label': 'checkbox with default color' }}
-      />
+        />
+        <h2 color='orange'>Orange Sherbet</h2> 
+      </div>
+      <div className='Herbal Meadows'>
+        <Switch inputProps={{ 'aria-label': 'primary checkbox' }} />
+        <h2 color='orange'>Herbal Meadows</h2> 
+      </div>
     </div>
   );
 }
